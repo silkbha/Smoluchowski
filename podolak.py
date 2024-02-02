ecd00689f0121acb62f3e0773ae236aed65406f6
silkbha <silkbha@gmail.com>
2024-02-02 17:01:34 +0100

    friday commit

--- podolak.py
+++ podolak.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-def vrel_brown(m_i,m_j,T_gas):
+def vrel_bm(m_i,m_j,T_gas):
     """ Brownian motion component to be added to relative particle velocities.
+        Source: Birnstiel et al. 2010 (A&A 513, A79), Eq. 44.
         Allows for treatment of collisions between same-sized particles, i.e. m_i == m_j.
-        (otherwise their relative velocities would be zero -> no collisions -> no coagulation/fragmentation)
+        (otherwise their relative velocities would always be zero -> no collisions -> no coagulation/fragmentation)
     """
     k_B = 1.380649e-16 # Boltzmann constant in CGS units (erg/K)
     # k_B = 1.380649e-23 # Boltzmann constant in SI units (J/K)
     return np.sqrt( (8 * (m_i+m_j) * k_B * T_gas) / (np.pi*m_i*m_j) )
 
-def sigma(i,j):
+def sigma(r_i,r_j):
     """ Collisional cross section.
+        #TODO units?
     """
-    return 1
+    return np.pi * (r_i+r_j)**2
 
 def C(m_i,m_j,m_k):
     
@@ -31,9 +33,12 @@ def C(m_i,m_j,m_k):
         return 0
 
 
-
+#################
+# Main Function #
+#################
 def podolak(dustinfo,duststate,gasstate):
-    """ Single-step time evolution of the Smoluchowski coagulation equation using the Podolak algorithm (Brauer et al. 2008).
+    """ Single-step time evolution of the Smoluchowski coagulation equation at one point in space using the Podolak algorithm.
+        Source: Brauer et al. 2008 (A&A 480, 859-877), Appendix A.1.
     
         Input:
             - dustinfo  = 2D array containing time-invariant information on dust size bins.
@@ -49,8 +54,12 @@ def podolak(dustinfo,duststate,gasstate):
         Output:
             - array of number density distribution at time t0 + dt
                 - row 1/1 : particle number density per size bin
+
+        #TODO: collisional cross-section radius units (maybe just input pre-calculated collisional cross section matrix?)
+        #TODO: C
     """
     
+    # Check if input dust info & state arrays are correctly given.
     if dustinfo.shape != duststate.shape:
         raise ValueError("Dust info and state arrays must contain the same number of bins.")
 
@@ -69,7 +78,7 @@ def podolak(dustinfo,duststate,gasstate):
     n_gas = gasstate[2]
     ########################################################################################
 
-    densities_new = np.zeros([len(St)])
+    densities_new = np.zeros([len(densities)])
     for k,(St_k,m_k,n_k,v_k) in enumerate(zip(St,masses,densities,velos)):
 
         dndt_gain = 0
@@ -77,14 +86,14 @@ def podolak(dustinfo,duststate,gasstate):
         for i,(St_i,m_i,n_i,v_i) in enumerate(zip(St,masses,densities,velos)):
 
             # Define relative velocity with added Browninan motion term.
-            vrel_ik = np.abs(v_k-v_i) + vrel_brown(m_k,m_i,T_gas)
+            vrel_ik = np.abs(v_k-v_i) + vrel_bm(m_k,m_i,T_gas)
 
             # Mass loss due to coagulation
             dndt_loss += n_k*n_i * sigma(St_k,St_i) * vrel_ik
             
             # Mass gain due to coagulation 
             for j,(St_j,m_j,n_j,v_j) in enumerate(zip(St,masses,densities,velos)):
-                vrel_ij = np.abs(v_j-v_i) + vrel_brown(m_j,m_i)
+                vrel_ij = np.abs(v_j-v_i) + vrel_bm(m_j,m_i)
                 dndt_gain += n_i*n_j * sigma(St_i,St_j) * vrel_ij * C(m_i,m_j,m_k)
         
         # Get new n_k(t+1) by adding dndt to previous n_k(t).
