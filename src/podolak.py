848536827adb4d3f5296dbdc2a02a00c8c091c56
silkbha <silkbha@gmail.com>
2024-03-04 17:10:45 +0100

    Monday commit: progress on modified Podolak

--- src/podolak.py
+++ src/podolak.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import heaviside as Theta
 
 def vrel_bm(m_i,m_j,T_gas):
     """ Brownian motion component to be added to relative particle velocities.
@@ -61,6 +62,37 @@ def C(masses,i,j,k):
     else:
         raise ValueError("Something went wrong when computing nearest neighboring mass bins!")
 
+def D(masses, j,k,c_e):
+    """ Calculates coefficient D_jk for the modified Podolak coagulation algorithm.
+        From: Brauer et al. 2008 (A&A 480, 859-877), Equation A.9.
+        # TODO: edge cases
+    """
+    if j <= k+1-c_e:
+        return -masses[j] / (masses[k+1] - masses[k])
+    else:
+        return -1
+
+def E(masses, j,k,c_e):
+    """ Calculates coefficient E_jk for the modified Podolak coagulation algorithm.
+        From: Brauer et al. 2008 (A&A 480, 859-877), Equation A.10.
+    """
+    return # TODO theta??
+
+def M(masses, i,j,k):
+    """ Calculates coefficient M_ijk for the modified Podolak coagulation algorithm.
+        From: Brauer et al. 2008 (A&A 480, 859-877), Equation A.13.
+        # TODO: edge cases, E, c_e
+    """
+    c_e = 1 # TODO what? / how?
+    M = C(masses,i,j,k) * Theta(k-i-1.5, 0) * Theta(i-j-0.5, 0)
+    if i == j:
+        M += 0.5 * C(masses,i,j,k)
+    if i == k:
+        M += D(masses,j,i,c_e)
+    if i == k-1:
+        M += E(masses,j,i+1,c_e) * Theta(k-j-1.5)
+    return M
+
 
 
 ###############################################################################################################
@@ -103,10 +135,12 @@ def evolve_simple(sizes,masses, densities,velos,T_gas):
             
             # Mass gain due to coagulation 
             for j,(r_j,m_j,n_j,v_j) in enumerate(zip(sizes,masses,densities,velos)):
-                vrel_ij = np.linalg.norm(v_j-v_i) + vrel_bm(m_j,m_i,T_gas)
+                
                 C_ijk = C(masses,i,j,k)
                 if C_ijk == 0:
                     continue # dndt_gain += 0
+
+                vrel_ij = np.linalg.norm(v_j-v_i) + vrel_bm(m_j,m_i,T_gas)
                 dndt_gain += n_i*n_j * sigma(r_i,r_j) * vrel_ij * C_ijk
         
         # Get new n_k(t+1) by adding dndt to previous n_k(t).
@@ -120,7 +154,6 @@ def evolve_modified(sizes,masses, densities,velos,T_gas):
     """ Single-step time evolution of the Smoluchowski coagulation equation in 0D using the Podolak algorithm.
         Modified version to accomodate larger value ranges with better numerical precision for mass conservation.
         Based on: Brauer et al. 2008 (A&A 480, 859-877), Appendix A.2.
-        TODO : everything
     
         Input:
             ========
@@ -141,28 +174,20 @@ def evolve_modified(sizes,masses, densities,velos,T_gas):
     densities_new = np.zeros(len(densities))
     for k,(r_k,m_k,n_k,v_k) in enumerate(zip(sizes,masses,densities,velos)):
 
-        dndt_gain = 0
-        dndt_loss = 0
+        dndt = 0
 
         for i,(r_i,m_i,n_i,v_i) in enumerate(zip(sizes,masses,densities,velos)):
-
-            # Define relative velocity with added Browninan motion term.
-            vrel_ik = np.linalg.norm(v_k-v_i) + vrel_bm(m_k,m_i,T_gas)
-
-            # Mass loss due to coagulation
-            dndt_loss += n_k*n_i * sigma(r_k,r_i) * vrel_ik
-            
-            # Mass gain due to coagulation 
             for j,(r_j,m_j,n_j,v_j) in enumerate(zip(sizes,masses,densities,velos)):
-                vrel_ij = np.linalg.norm(v_j-v_i) + vrel_bm(m_j,m_i,T_gas)
-                C_ijk = C(masses,i,j,k)
-                if C_ijk == 0:
+                
+                M_ijk = M(masses,i,j,k)
+                if M_ijk == 0:
                     continue # dndt_gain += 0
-                dndt_gain += n_i*n_j * sigma(r_i,r_j) * vrel_ij * C_ijk
-        
+
+                vrel_ij = np.linalg.norm(v_j-v_i) + vrel_bm(m_j,m_i,T_gas)
+                dndt += n_i*n_j * sigma(r_i,r_j) * vrel_ij * M_ijk
+
         # Get new n_k(t+1) by adding dndt to previous n_k(t).
-        # (with factor 0.5 in gain term to prevent double counting of collisions ij & ji)
-        densities_new[k] = n_k # + 0.5 * dndt_gain - dndt_loss
+        densities_new[k] = n_k + dndt
     
     return densities_new
 
