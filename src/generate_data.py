e1a6c5fc68c669b3949207ca9964777e72ea5588
silkbha <silkbha@gmail.com>
2024-03-18 20:59:28 +0100

    monday schmonday

--- src/generate_data.py
+++ src/generate_data.py
@@ -10,28 +10,34 @@ def MRN(a, amin,amax,rho_gas, r=3.5):
     return (4-r) * a**(-r) / (4/3 * np.pi * da * rho_gas)
 
 
-def generate_inputs(St_min,St_max, idxmin0,idxmax0, rho_dust,rho_gas,c_s):
+def generate_inputs(nbins,idxmin0,idxmax0 ,rho_gas,c_s):
+    """ # First try: zero vrel, brownian only : check analytical solution dullemond/dominik 2005.
+    """
+    rho_dust = 1e1
+    St_min, St_max = 0.01, 0.5
+    Stokes = np.logspace(St_min,St_max,nbins)
 
-    # Stays constant throughout parameter sweep
-    Stokes = np.logspace(St_min,St_max,100)
-    sizes,masses,T_gas = prep(Stokes,rho_dust,rho_gas,c_s)
-
+    sizes, masses, T_gas = prep(Stokes,rho_dust,rho_gas,c_s)
     densities = MRN(sizes, sizes[idxmin0],sizes[idxmax0], rho_gas)
-    # Variable parameters (ranges)
-    # TODO number density distributions: MRN
-    # TODO velos: input each 3D particle velocity or compute relative velo matrix first?
+    velos = np.zeros((len(Stokes),3))
 
-    # TODO gas parameters: single values, easy to set ranges
-
-    # vary over MRN distribution!! minmax, slope
-    # Loop over variable param grid!
-
-
-
-    # First try: zero vrel, brownian only : check analytical solution dullemond/dominik 2005
-    return sizes, masses, densities, T_gas
+    return sizes, masses, densities, velos, T_gas
 
 def parameter_sweep():
+
+    nbins   = 100
+
+    Rho_gas = np.logspace(0.1,10,100)
+    C_s     = np.logspace(0.1,1,100)
+    
+    for rho_gas in Rho_gas:
+        for c_s in C_s:
+            for idxmin0 in range(nbins):
+                for idxmax0 in range(nbins):
+                    sizes,masses,densities,velos,T_gas = generate_inputs(nbins,idxmin0,idxmax0,rho_gas,c_s)
+                    densities_new = evolve(sizes,masses, densities,velos,T_gas)
+    #TODO save, track, etc.
+    
     return
 
 
