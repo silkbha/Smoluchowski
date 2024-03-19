8b4bb1dfc3477f7fa3998550f63a691bf201e369
silkbha <silkbha@gmail.com>
2024-03-19 10:38:48 +0100

    testy schmesty

--- src/generate_data.py
+++ src/generate_data.py
@@ -9,6 +9,8 @@ def MRN(a, amin,amax,rho_gas, r=3.5):
     da = amax**(4-r) - amin**(4-r)
     return (4-r) * a**(-r) / (4/3 * np.pi * da * rho_gas)
 
+# def full_evolution(sizes,masses, densities,velos,T_gas):
+#     return
 
 def generate_inputs(nbins,idxmin0,idxmax0 ,rho_gas,c_s):
     """ # First try: zero vrel, brownian only : check analytical solution dullemond/dominik 2005.
@@ -24,7 +26,8 @@ def generate_inputs(nbins,idxmin0,idxmax0 ,rho_gas,c_s):
     return sizes, masses, densities, velos, T_gas
 
 def parameter_sweep():
-
+    """
+    """
     nbins   = 100
 
     Rho_gas = np.logspace(0.1,10,100)
@@ -40,6 +43,22 @@ def parameter_sweep():
     
     return
 
+def test():
+    """
+    """
+    nbins   = 100
+    steps   = 100
+    idxmin0 = 1
+    idxmax0 = 10
+    rho_gas = 1e-6
+    c_s     = 1e1
+
+    densities = np.zeroes((nbins,steps))
+    sizes,masses, densities[:,0], velos, T_gas = generate_inputs(nbins,idxmin0,idxmax0,rho_gas,c_s)
+    for k in range(steps-1):
+        densities[:,k+1] = evolve(sizes,masses, densities[:,k],velos,T_gas)
+
+    return
 
 if __name__=="__main__":
     parameter_sweep()
