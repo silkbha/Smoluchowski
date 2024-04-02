f4a1029402954382d9e447cb9c11d3b72e8a0104
silkbha <silkbha@gmail.com>
2024-04-02 16:34:53 +0200

    Start playing with dustpy

---
+++ src/dustpy_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import dustpy as dp
+from matplotlib import pyplot as plt
+
+def test():
+    """
+    """
+    
+    return
+
+if __name__=="__main__":
+    print("dustpy test")
+    test()
