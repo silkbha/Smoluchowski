21c84a6475a584a997dfc626c47f6e3c76c8214a
silkbha <silkbha@gmail.com>
2024-06-17 23:17:02 +0200

    toy model!!

--- src/generate_data.py
+++ src/generate_data.py
@@ -21,7 +21,7 @@ def parameter_sweep(output_dir, kernel):
     # Floating point parameters to sweep
     a  = np.array([1.]) # np.linspace(0.1,10,50) #
     S0 = np.logspace(-4,3,200)
-    t  = np.linspace(1e-9,1e3,20000)
+    t  = np.logspace(-9,3,20000)
     
     par_space = len(a)*len(S0)*len(t)
     print("Param space size:", par_space)
@@ -39,11 +39,29 @@ def parameter_sweep(output_dir, kernel):
     filename = f"{output_dir}/data_{kernel}"
     with h5py.File(filename+".h5", "w") as hf:
         hf.create_dataset(f"data_{kernel}", data=data)
-    # np.savetxt(filename+"dat", data)
     print("Done and Saved!")
-
     return
 
+
+
+def simple_dataset(output_dir):
+    """ 
+    """
+    m  = np.logspace(-12,4,100)
+    t  = np.logspace(-9,3,1000)
+
+    cols = len(m) + 1
+    data = np.zeros((len(t), cols))
+
+    for i,x in enumerate(t):
+        data[i,0] = x
+        data[i,1:]  = solution_constant_kernel(m,1.,1.,x)
+    
+    filename = f"{output_dir}/simple_dataset"
+    with h5py.File(filename+".h5", "w") as hf:
+        hf.create_dataset("simple_dataset", data=data)
+    print("Done and Saved!")
+    return
 
 
 if __name__ == "__main__":
@@ -55,8 +73,9 @@ if __name__ == "__main__":
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     print(f"Saving data in directory: {output_dir}")
 
-    parameter_sweep(output_dir, "constant")
-    parameter_sweep(output_dir, "linear")
+    # parameter_sweep(output_dir, "constant")
+    # parameter_sweep(output_dir, "linear")
+    simple_dataset(output_dir)
 
     end = time.time()
     print(f"Elapsed time: {end-start} s")
--- src/toy_model.py
+++ src/toy_model.py
@@ -1,2 +1,119 @@
+import time
+import os
+from pathlib import Path
+import h5py
+
 import numpy as np
+import torch
 from torch import nn
+import torch.optim as optim
+import torch.utils.data as data
+
+
+def create_dataset(dataset, lookback):
+    """ Transform a time series into a prediction dataset.
+        Source: https://machinelearningmastery.com/lstm-for-time-series-prediction-in-pytorch/
+        
+        Args:
+            dataset: A numpy array of time series, first dimension is the time steps
+            lookback: Size of window for prediction
+    """
+    X, y = [], []
+    for i in range(len(dataset)-lookback):
+        feature = dataset[i:i+lookback]
+        target = dataset[i+1:i+lookback+1]
+        X.append(feature)
+        y.append(target)
+    X,y = np.array(X), np.array(y)
+    return torch.tensor(X), torch.tensor(y)
+
+
+
+class ToyModel(nn.Module):
+    """
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.input_size = 100
+        self.hidden_size = 512
+        self.output_size = self.input_size
+        self.lstm = nn.LSTM(input_size=self.input_size,
+                            hidden_size=self.hidden_size,
+                            batch_first=True,
+                            num_layers = 2,
+                            dtype=torch.float64
+                            )
+        self.out = nn.Linear(self.hidden_size, self.output_size, dtype=torch.float64)
+    
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.out(x)
+        return x
+
+
+
+
+def main(filename, output_dir):
+    f = h5py.File(filename, "r")
+    # t      = f["simple_dataset"][:,0]
+    series = f["simple_dataset"][:,1:]
+    print(len(series))
+
+    train_size = int(len(series) * 0.67)
+    # test_size = len(series) - train_size
+    train,test = series[:train_size], series[train_size:]
+
+    lookback = 1
+    X_train , y_train = create_dataset(train, lookback)
+    X_test , y_test = create_dataset(test, lookback)
+
+    print(X_train.shape, y_train.shape)
+    print(X_test.shape, y_test.shape)
+    print(X_train.dtype, y_train.dtype)
+    print(X_test.dtype, y_test.dtype)
+
+    model = ToyModel()
+    optimizer = optim.Adam(model.parameters())
+    loss_fn = nn.MSELoss()
+    loader = data.DataLoader(data.TensorDataset(X_train, y_train), shuffle=False, batch_size=8)
+
+    n_epochs = 250
+    for epoch in range(n_epochs):
+        model.train()
+        for X_batch, y_batch in loader:
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # Validation
+        if epoch % 25 != 0:
+            continue
+        model.eval()
+        with torch.no_grad():
+            y_pred = model(X_train)
+            train_rmse = np.sqrt(loss_fn(y_pred, y_train))
+            y_pred = model(X_test)
+            test_rmse = np.sqrt(loss_fn(y_pred, y_test))
+        print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+
+    torch.save(model.state_dict(), output_dir+"/toymodel.pt")
+
+    return
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+
+    src_dir = os.path.dirname(os.path.abspath(__file__))
+    main_dir = str(Path(src_dir).parents[0])
+    data_dir = os.path.join(main_dir, "data")
+    model_dir = os.path.join(main_dir, "models")
+
+    filename = os.path.join(data_dir, "simple_dataset.h5")
+
+    main(filename, model_dir)
+
+    end = time.time()
+    print(f"Elapsed time: {end-start} s")
